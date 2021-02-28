85979e9731017c6aca821fa37a722138cabb409a
Aleksandr kozachenko <dev.kozachenko@gmail.com>
2021-02-28 10:29:41 +0200

    git pages

--- src/js/lib/addHidden.js
+++
@@ -1,8 +0,0 @@
-function addHidden(event) {
-    if(event.target === event.currentTarget) {
-    event.target.style.display = 'none';
-    document.body.style.overflow = 'visible';
-    }
-  }
-
-  export default addHidden;
--- src/js/lib/createElem.js
+++
@@ -1,7 +0,0 @@
-function createElem(element, myClass) {
-    const newElem = document.createElement(element);
-    newElem.classList.add(myClass);
-    return newElem;
-}
-
-export default createElem;
